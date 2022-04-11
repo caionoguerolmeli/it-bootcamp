35d4d10b86be65e84bfbe8587ebc4c18081a2d36
caionoguerolmeli <caio.motta@mercadolivre.com>
2022-04-11 17:21:25 -0300

    add datastorage exercise

---
+++ data-storage3/qa-bugs/src/test/java/br/com/meli/qabugs/QaBugsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.qabugs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaBugsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
